68a9a5124ae1d6fe149286fc42d6e7ddcc679210
Satoru Takeuchi <satoru.takeuchi@gmail.com>
2020-04-02 08:07:20 +0000

    update
    Signed-off-by: Satoru Takeuchi <satoru.takeuchi@gmail.com>

--- init.sh
+++ init.sh
@@ -2,8 +2,8 @@
 
 kind create cluster --config kind-cluster.yaml
 kubectl label node kind-worker tsc=1
-kubectl label node kind-worker tsc=2
-kubectl label node kind-worker tsc=3
+kubectl label node kind-worker2 tsc=2
+kubectl label node kind-worker3 tsc=3
 kubectl apply -f sc.yaml
 kubectl apply -f testpv.yaml
 kubectl apply -f ceph/common.yaml
